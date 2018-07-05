3f853785b11ba351749dd6deaf991f25a34f05e4
Matt Driscoll <MDriscoll@esri.com>
2018-07-05 14:13:35 -0700

    consistency, updated view

---
+++ demos/updated-view/app/main.js
@@ -0,0 +1,24 @@
+define(["require", "exports", "esri/config", "esri/Map", "esri/views/MapView", "./WebMapShowcase"], function (require, exports, config, Map, MapView, WebMapShowcase) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    // disable prompting
+    config.request.useIdentity = false;
+    //----------------
+    //  map setup
+    //----------------
+    var map = new Map({
+        basemap: "streets-vector"
+    });
+    var view = new MapView({
+        map: map,
+        container: "viewDiv",
+        center: [-117.1628487109789, 32.706813240831096],
+        zoom: 15
+    });
+    //----------------
+    //  widget setup
+    //----------------
+    var widget = new WebMapShowcase();
+    view.ui.add(widget, "top-right");
+});
+//# sourceMappingURL=main.js.map
